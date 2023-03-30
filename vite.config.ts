276dc3e3c0ab574a3771d501b9423572d1b5d00b
knull-a <kenjopa6@gmail.com>
2023-03-30 14:58:16 +0500

    remove localhost port

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  server: {
-    port: 3000,
-    strictPort: true
-  }
+  } 
 })
